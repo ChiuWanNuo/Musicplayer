2701a7cd83a97aa85d3f902600487c306c71241b
Wan-Nuo, Chiu <sunnusbi@gmail.com>
2020-04-01 20:48:54 +0100

    Initial Commit

---
+++ Musicplayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Musicplayer
+//
+//  Created by ChiuWanNuo on 01/04/2020.
+//  Copyright © 2020 ChiuWanNuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
